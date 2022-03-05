c279e0d595bc62195c1956973827fb2c22a87a44
Joe Curtis <joecurtis9999@gmail.com>
2022-03-05 15:41:19 +0000

    added engineer tests to test github val and role

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -5,3 +5,15 @@ test('creates an Engineer object', () => {
 
     expect(engineer.github).toEqual(expect.any(String));
 });
+
+test('gets engineer github value', () => {
+    const engineer = new Engineer('Joseph', 89, 'joecurtis9999@gmail.com');
+
+    expect(engineer.getGithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+test('gets role of employee', () => {
+    const engineer = new Engineer('Joseph', 89, 'joecurtis9999@gmail.com');
+    
+    expect(engineer.getRole()).toEqual("Engineer");
+});
