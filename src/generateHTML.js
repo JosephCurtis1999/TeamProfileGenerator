004fa50ce54e941037cdc073cefa64e6a883952b
Joe Curtis <joecurtis9999@gmail.com>
2022-03-05 11:50:05 +0000

    generate Intern function

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -15,8 +15,8 @@ const generateManager = function (manager) {
             </div>
         </div>
     </div>
-    `;
-}
+    `
+};
 
 const generateEngineer = function (engineer) {
     return `
@@ -34,4 +34,23 @@ const generateEngineer = function (engineer) {
         </div>
     </div>
     `
-}
+};
+
+const generateIntern = function (intern) {
+    return `
+    <div class="col-xs-12 col-md-12 mt-4">
+        <div class="card h-100">
+            <div class="card-header">
+                <h3>${intern.name}</h3>
+                <h4>Intern</h4><i class="material-icons">assignment_ind</i>
+            </div>
+            <div class="card-body">
+                <p class="id">ID: ${intern.id}</p>
+                <p class="email">Email:<a href="mailto:${intern.email}">${intern.email}</a></p>
+                <p class="school">School: ${intern.school}</p>
+            </div>
+    </div>
+</div>
+`
+};
+
