1388449034bcaa699a9a7e740d82332bc94c1d0d
Joe Curtis <joecurtis9999@gmail.com>
2022-03-05 15:45:07 +0000

    finished intern tests

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,8 +1,19 @@
 const Intern = require('../lib/Intern');
 
 test('creates an Intern object', () => {
-    const intern = new Intern('Nicole', 90, 'nicole.elisaw@gmail', 'SFSU');
+    const intern = new Intern('Joseph', 89, 'joecurtis9999@gmail.com', 'UOB');
     
     expect(intern.school) .toEqual(expect.any(String));
 });
 
+test('gets employee school', () => {
+    const intern = new Intern('Joseph', 89, 'joecurtis9999@gmail.com', 'UOB');
+    
+    expect(intern.getSchool()).toEqual(expect.stringContaining(intern.school.toString()));
+});
+
+test('gets role of employee', () => {
+    const intern = new Intern('Joseph', 89, 'joecurtis9999@gmail.com', 'UOB');
+
+    expect(intern.getRole()).toEqual("Intern");
+}); 
