47d50e1b58d629c936ae5c49ec27941ae05c79ff
Joe Curtis <joecurtis9999@gmail.com>
2022-03-05 13:27:50 +0000

    prompts for the manager

--- index.js
+++ index.js
@@ -12,4 +12,70 @@ const teamArray = [];
 
 // questions, prompts etc, similar to questions such in readmegenerator
 
+// prompts for the manager
+const addManager = () => {
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Who is the manager of this team?', 
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log ("Please enter the manager's name!");
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'id',
+        message: "Please enter the manager's ID.",
+        validate: idInput => {
+            if  (isNaN(idInput)) {
+                console.log ("Please enter the manager's ID!")
+                return false; 
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: "Please enter the manager's email.",
+        validate: email => {
+            valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+            if (valid) {
+                return true;
+            } else {
+                console.log ('Please enter an email!')
+                return false; 
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'officeNumber',
+        message: "Please enter the manager's office number",
+        validate: officeNb => {
+            if  (isNaN(officeNb)) {
+                console.log ('Please enter an office number!')
+                return false; 
+            } else {
+                return true;
+            }
+        }
+    }
+]) 
+.then(managerInput => {
+    //creates new manager
+    const {name,id,email,officeNumber} = managerInput;
+    const manager = new Manager (name,id,email,officeNumber)
 
+    teamArray.push(manager)
+    console.log(manager)
+
+})
+}
